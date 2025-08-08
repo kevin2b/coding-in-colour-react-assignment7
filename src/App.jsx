75bcc7e645e48ec4ad0f2fcd86de1e1366415b9f
kevin2b <25062561+kevin2b@users.noreply.github.com>
2025-08-08 10:50:33 -0400

    Add previous vanilla HTML/JS/CSS solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 /** Component Imports */
-import Example from './components/Example'
+import star from '@images/icon-star.svg';
+import thankYou from '@images/illustration-thank-you.svg';
 
 /**
  * This is the entry point for your react-app
@@ -15,33 +16,46 @@ import Example from './components/Example'
 
 const App = () => {
   return (
-    <div>
-      {<Example /> /** You can remove this when you wish, it's provided as an example of modular styling */}
-      
-      {/** Rating state start **/}
+    <article class="wrapper">
+      <div class="card card-1 js-card-1">
+        <span class="circle-container">
+          <img src={star} alt="" class="circle__star" />
+        </span>
+        <h1 class="card__header">How did we do?</h1>
+        <p class="card__text">
+          Please let us know how we did with your support request. All feedback
+          is appreciated to help us improve our offering!
+        </p>
+        <section class="rating-group">
+          <button class="rating rating--hover js-rating">1</button>
+          <button class="rating rating--hover js-rating">2</button>
+          <button class="rating rating--hover js-rating">3</button>
+          <button class="rating rating--hover js-rating">4</button>
+          <button class="rating rating--hover js-rating js-rating--select">
+            5
+          </button>
+        </section>
+        <button class="card__submit js-card__submit card__submit--hover">
+          Submit
+        </button>
+      </div>
 
-      How did we do?
-
-      Please let us know how we did with your support request. All feedback is appreciated 
-      to help us improve our offering!
-
-      1 2 3 4 5
-
-      Submit
-
-      {/** Rating state end **/}
-
-      {/** Thank you state start **/}
-
-      You selected {/** Add rating here **/} out of 5
-
-      Thank you!
-
-      We appreciate you taking the time to give a rating. If you ever need more support, 
-      don’t hesitate to get in touch!
-
-      {/** Thank you state end **/}
-    </div>
+      <div class="card card-2 js-card-2 js-hidden">
+        <img
+          src={thankYou}
+          alt=""
+          class="card__img"
+        />
+        <p class="card__selected">
+          You selected <span class="card__rating-value"> </span> out of 5
+        </p>
+        <h1 class="card__header">Thank you!</h1>
+        <p class="card__text">
+          We appreciate you taking the time to give a rating. If you ever need
+          more support, don't hesitate to get in touch!
+        </p>
+      </div>
+    </article>
   )
 }
 
