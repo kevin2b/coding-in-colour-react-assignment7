66121aae1713d8e8bac98a14500dca91c6953d1f
kevin2b <25062561+kevin2b@users.noreply.github.com>
2025-08-08 21:28:27 -0400

    Fix CSS to match design more

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
   root: 'src',
   plugins: [react()],
-  build: {
-    outDir: '../dist',
-    emptyOutDir: true
-  }
+  resolve: {
+    alias: {
+      '@images': path.resolve(__dirname, 'images'),
+    }
+  },
 })
